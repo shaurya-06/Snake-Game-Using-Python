a320508c4649e22f7fa16ddf21ee4f995e0680aa
Shaurya Singh <58339737+shaurya-06@users.noreply.github.com>
2020-11-28 21:39:48 +0530

    added comments in main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from food import Food
 from scoreboard import ScoreBoard
 import time
 
+# Setting up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -15,6 +16,7 @@ snake = Snake()
 food = Food()
 scoreboard = ScoreBoard()
 
+# Adding eventlisteners on the screen
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -25,12 +27,14 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    
+    # Checking if snake has made contact with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
         snake.extend()
 
+    # Checking if snake has made contact with walls
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
         snake.reset()
